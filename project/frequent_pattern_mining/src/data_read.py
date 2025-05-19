1c8c693be6573859a8af98b9e1f28a6ca9e1ae6b
Heltion <heltionartist@gmail.com>
2025-05-19 20:21:21 +0800

    Update Code

---
+++ project/frequent_pattern_mining/src/data_read.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import pandas as pd
+import json
+import time
+
+
+def get_parquet_files(data_file, part_num):
+	parquet_files = []
+	for i in range(0, part_num):
+		parquet_file = os.path.join(data_file, f"part-{i:05d}_purchase_history.parquet")
+		parquet_files.append(parquet_file)
+	return parquet_files
+
+
+def data_purchase_history_items_sub(data_file, part_num):
+	for ind, parquet_file in enumerate(get_parquet_files(data_file, part_num)):
+		print(f"Reading {parquet_file}...")
+		data = pd.read_parquet(parquet_file)
+		print(data)
+		del data
+
+
+def data_purchase_history_items():
+	data_purchase_history_items_sub("data/30G_data_new", 1)
+
+
+def main():
+	start_time = time.time()
+	data_purchase_history_items()
+	end_time = time.time()
+	execution_time = end_time - start_time
+	print(f"Execution time: {execution_time:.2f}s")
+
+
+if __name__ == '__main__':
+	main()
