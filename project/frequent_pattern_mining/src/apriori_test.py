77b4ee20375d2969e4760177bff7cef790042b60
Heltion <heltionartist@gmail.com>
2025-05-20 14:52:21 +0800

    Update Code

--- project/frequent_pattern_mining/src/apriori_test.py
+++ project/frequent_pattern_mining/src/apriori_test.py
@@ -10,7 +10,8 @@ import pandas as pd
 
 def apriori_process_sub(data_file, support_threshold, confidence_threshold):
 	parquet_file = os.path.join(data_file, "data_categories.parquet")
-	data = pd.read_parquet(parquet_file)
+	n = 135000
+	data = pd.read_parquet(parquet_file).head(n)
 	# 使用Apriori算法找到频繁项集
 	frequent_itemsets = apriori(data, min_support=support_threshold, use_colnames=True)
 	# 生成关联规则
@@ -19,6 +20,7 @@ def apriori_process_sub(data_file, support_threshold, confidence_threshold):
 	print("频繁项集:")
 	print(frequent_itemsets)
 	print("\n关联规则:")
+	print(rules)
 	print(rules[['antecedents', 'consequents', 'confidence']])
 	del data, frequent_itemsets, rules
 
@@ -26,8 +28,6 @@ def apriori_process_sub(data_file, support_threshold, confidence_threshold):
 def apriori_process():
 	print("Real data:")
 	apriori_process_sub("data/30G_data_new", 0.02, 0.4)
-	print("Random data:")
-	apriori_process_sub("data/random_data", 0.02, 0.4)
 
 
 def main():
